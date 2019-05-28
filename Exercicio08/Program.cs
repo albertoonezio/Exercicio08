627a8bd9e6fce9b4017443a6bc9c13a6c4d7ac11
Alberto <albertomaronezio@hotmai.com>
2019-05-28 16:55:09 -0300

    Exercicio08

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, endereco, telefone = "";
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Digite seu Endereço: ");
+            endereco = Console.ReadLine();
+            Console.Write("Digite seu Telefone: ");
+            telefone = Console.ReadLine();
+
+            Console.WriteLine("Seu Nome é: {0}, e você mora em: {1}, com o telefone: {2}", nome, endereco, telefone);
+        }
+    }
+}
